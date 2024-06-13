f781d7e2c0ee7b3a7a0636d1949c85bde1ca5504
Thomas L'Esperance <144936700+thomaslesperance@users.noreply.github.com>
2024-06-13 15:59:09 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,15 @@
+# main.py
+'''
+import arguments
+import other files
+initialize class obj.s
+Infinite loop: {
+check-time.py
+If true: {
+check-stock.py
+If true: {
+email-notification.py
+}
+}
+}
+'''
