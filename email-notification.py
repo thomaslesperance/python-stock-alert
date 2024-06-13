bc0359b0a07e98d1976999d23000559f82894985
Thomas L'Esperance <144936700+thomaslesperance@users.noreply.github.com>
2024-06-13 16:01:00 -0500

    Create email-notification.py

---
+++ email-notification.py
@@ -0,0 +1,13 @@
+# email-notification.py
+'''
+import 
+stock data params
+email login params
+email template field
+email login field
+get email login method
+set email login method
+get email template method
+create SMTP con. method
+send alert method
+'''
