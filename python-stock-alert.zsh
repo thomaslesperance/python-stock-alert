9f49748607ce0ef8e17df1bb3b965487e5a6a0e0
Thomas L'Esperance <144936700+thomaslesperance@users.noreply.github.com>
2024-06-13 17:18:27 -0500

    Update python-stock-alert.zsh

--- python-stock-alert.zsh
+++ python-stock-alert.zsh
@@ -1,5 +1,16 @@
 # Zsh script should:
-# allow ctrl + c to abort
 # offer help info
 # be added to PATH
-# pass CLI  arguments to python script
+# pass CLI arguments to python script
+
+#! /bin/zsh
+
+# This script runs the python script with the same filename.
+# Be sure to run the following command for the finished
+# file: ‘chmod u+x python-script.command’.
+# Save this file in a folder that is part of your PATH.
+# To verify what is in your PATH, use ‘echo $PATH’.
+# On MacOS, run script with ‘cmd-space’ for spotlight then 
+# type ‘python-script’.
+
+python3 /path/to/your/main.py
